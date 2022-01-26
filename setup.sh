ae88b55b0c220c6d93ad38d40c5d62f46f71a063
PalmerVincent <vincent.palmer@hotmail.com>
2022-01-26 20:38:06 +0100

    Trying with setup

--- setup.sh
+++ setup.sh
@@ -1,6 +1,41 @@
 #!/bin/bash
 
+# Setup
+
+echo "What is the Pi number?"
+read nbr
+
+address="inutiuser@inuti$nbr.lab.eit.lth.se"
+repo="git@github.com:PalmerVincent/EITN30.git"
+
+# Copy key to Pi 
+
+ssh-copy-id -i ~/.ssh/eitn30-pi.pub $address
+
+# Start ssh
+
+echo -e "LangGeNot5G" | ssh $address 
+
+# Configure git 
+
 git config --global user.email "vincent.palmer@hotmail.com"
-git config --global user.name "PalmerVincent"
+git config --global user.name "InternetInuti"
+
+# Fix Git repo
+
+if [ -d "~/EITN30" ]
+then 
+	echo "Repo exists, Updating"
+	cd ~/EITN30
+	git pull
+else
+	echo "Repo missing, Cloning"
+	git clone $repo
+
+fi
+
+cd ~/EITN30
+
+# Installing dependencies
 
 
