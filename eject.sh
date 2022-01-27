1fa303d0a36d28d6635b28fd33828c7439afce53
InternetInuti <vincent.palmer@hotmail.com>
2022-01-27 13:51:31 +0100

    Testing eject from rpi

---
+++ eject.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Script for pushing changes to repo and removing local files on RPi
+
+git add .
+git commit -m "Commit before cleaning"
+git push
+ 
+cd ..
+rm -rf EITN30
+
+exit
+
--- setup.sh
+++ setup.sh
@@ -1,42 +1,21 @@
 #!/bin/bash
 
-# Setup
+# Setting up RPi
 
-echo "What is the Pi number?"
-read nbr
 
-address="inutiuser@inuti$nbr.lab.eit.lth.se"
+# address="inutiuser@inuti$nbr.lab.eit.lth.se"
 repo="git@github.com:PalmerVincent/EITN30.git"
 
-# Copy key to Pi 
-
-ssh-copy-id -i ~/.ssh/eitn30-pi.pub $address
-
-# Start ssh
-
-# echo "LangGeNot5G" | ssh $address 
 
 # Configure git 
 
-# git config --global user.email "vincent.palmer@hotmail.com"
-# git config --global user.name "InternetInuti"
+git config --global user.email "vincent.palmer@hotmail.com"
+git config --global user.name "InternetInuti"
 
-# Fix Git repo
-
-# if [ -d "~/EITN30" ]
-# then 
-#	echo "Repo exists, Updating"
-#	cd ~/EITN30
-#	git pull
-# else
-#	echo "Repo missing, Cloning"
-#	git clone $repo
-
-# fi
-
-# cd ~/EITN30
 
 # Installing dependencies
 
 
-echo "hej"
+
+
+echo "Done with setup!"
