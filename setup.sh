c5ddd8860d8f5e9b94ff2e0e29d82e1d02708415
InternetInuti <vincent.palmer@hotmail.com>
2022-01-27 18:29:39 +0000

    Commit before cleaning

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@
 
 # address="inutiuser@inuti$nbr.lab.eit.lth.se"
 repo="git@github.com:PalmerVincent/EITN30.git"
-
+pipe="echo LangGeNot5G |"
 
 # Configure git 
 
@@ -14,11 +14,11 @@ git config --global user.name "InternetInuti"
 
 
 # Installing dependencies
-#echo -e "LangGeNot5G" | sudo apt-get update
-#sudo apt-get upgrade 
+$pipe sudo -s apt-get update
+$pipe sudo -s apt-get upgrade 
 
-echo -e "LangGeNot5G" | sudo pip3 install Adafruit-Blinka
-pip3 install circuitpython-nrf24l01
+$pipe sudo pip3 install Adafruit-Blinka
+$pipe sudo pip3 install circuitpython-nrf24l01
 
 
 echo "Done with setup!"
