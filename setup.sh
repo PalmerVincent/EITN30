d074644f558fb893ac9778a32ea55c510fd0416d
InternetInuti <vincent.palmer@hotmail.com>
2022-02-08 16:21:02 +0100

    Changing library

--- setup.sh
+++ setup.sh
@@ -33,3 +33,43 @@ else
     cd ~/git
     git clone $repo
 fi
+
+
+# Installing dependancies
+
+if [[ -f "/usr/include/RF24/" ]]
+then
+    echo "Library exists"
+else
+    cd ~/git/EITN30/
+    wget "https://github.com/nRF24/RF24/releases/download/v1.4.2/librf24-RPi_1.4.2-1_arm64.deb"
+    sudo dpkg -i librf24-RPi*arm64.deb
+    rm librf24-RPi*arm64.deb
+    echo "Library is installed"
+fi
+
+echo $pass | sudo -S apt-get install python3-dev libboost-python-dev python3-pip python3-rpi.gpio build-essentials
+
+python3 -m pip install --upgrade pip setuptools
+
+
+echo $pass | sudo -S ln -s $(ls /usr/lib/$(ls /usr/lib/gcc | tail -1)/libboost_python3*.so | tail -1) /usr/lib/$d /usr/lib/gcc | tail -1)/libboost_python3.so
+
+
+# Navigating for cloning repo, building wrapper
+
+cd ~/git/EITN30/
+
+git clone https://github.com/nRF24/RF24.git
+
+cd RF24/pyRF24/
+
+python3 setup.py build
+
+echo "Build complete"
+
+sudo python3 setup.py install
+
+echo "Install complete"
+
+
