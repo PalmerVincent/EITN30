c3a811cfcd1aa3a31dcf827203cbb56ee8c1cb36
Lukas Elmlund <lukas.elmlund@gmail.com>
2022-02-08 18:41:02 +0100

    setup

--- setup.sh
+++ setup.sh
@@ -57,9 +57,8 @@ python3 -m pip install --upgrade pip setuptools
 
 
 
-if [[ -f "/usr/lib/arm-linux-gnueabihf/libboost_python3.so"]]
-then
-    rm "/usr/lib/arm-linux-gnueabihf/libboost_python3.so"
+if [[ -e "/usr/lib/arm-linux-gnueabihf/libboost_python3.so"]]; then
+    sudo rm "/usr/lib/arm-linux-gnueabihf/libboost_python3.so"
 fi
 
 sudo ln -s $(ls /usr/lib/$(ls /usr/lib/gcc | tail -1)/libboost_python3*.so | tail -1) /usr/lib/$(ls /usr/lib/gcc | tail -1)/libboost_python3.so
