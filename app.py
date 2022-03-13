3f5bf3f57ac94b25aba065960efa416e7cfd8284
Vincent Palmer <vincentpalmernordfors@gmail.com>
2022-03-13 20:23:45 +0100

    Started with constructing packets to be sent

--- app.py
+++ app.py
@@ -173,9 +173,44 @@ def node():
     txThread.join()
 
 
-def create_packet():
-    pass
+def create_packet(dest: str, data: list):
+    
+    header = {
+            "VERSION": 0b0100, # 4 bits
+            "IHL": 0b0101, # 4 bits
+            "DSCP": 0b000000, # 6 bits
+            "ECN": 0b00, # 2 bits
+            "TotLen": 0x003c, # 2 bytes 
+            "Identification": 0x2c2d, # 2 bytes
+            "Flags": 0b000, # 3 bits
+            "FragmentOffset": 0b0000000000000, # 13 bits
+            "TTL": 0x80, # 1 byte
+            "Protocol": 0x01, # 1 byte
+            "Checksum": 0x0000, # 2 bytes
+            "Source": 0x0000, # 4 bytes
+            "Destination": 0x0000 # 4 bytes
+    }
+    
+    header["Destination"] = bytes(map(int, dest.split(".")))
+    
+    header_bytes = [
+        ((header["VERSION"] << 4) + header["IHL"] << 8) + ((header["DSCP"] << 2) + header["ECN"]),
+        header["TotLen"],
+        header["Identification"],
+        ((header["Flags"] << 13) + header["FragmentOffset"]), 
+        (header["TTL"] << 8) + header["Protocol"],
+        header["Checksum"],
+        header["Source"] & 0xFFFF,
+        header["Destination"]
+    ]
 
+    payload = data
+    
+    header_bytes.append(bytes(payload))
+    
+    packet = header_bytes
+    
+    return packet
 
 def fragment(data):
     """ Fragments incoming binary data in bytes
--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import struct
 import random
 
-# in
+
 def fragment(data):
     
     fragments = []
@@ -30,7 +30,7 @@ def fragment(data):
     return fragments   
 
 
-if __name__ == '__main__':
+def fragmentTest():
     data = [random.randint(0, 255) for _ in range(100)]
     print(bytes(data))
     print(data)
@@ -50,4 +50,47 @@ if __name__ == '__main__':
         print('\n')
     
     print(sum)
-      
+
+def packetTest():
+    header = {
+            "VERSION": 0b0100, # 4 bits
+            "IHL": 0b0101, # 4 bits
+            "DSCP": 0b000000, # 6 bits
+            "ECN": 0b00, # 2 bits
+            "TotLen": 0x003c, # 2 bytes 
+            "Identification": 0x2c2d, # 2 bytes
+            "Flags": 0b000, # 3 bits
+            "FragmentOffset": 0b0000000000000, # 13 bits
+            "TTL": 0x80, # 1 byte
+            "Protocol": 0x01, # 1 byte
+            "Checksum": 0x0000, # 2 bytes
+            "Source": 0x0000, # 4 bytes
+            "Destination": 0x0000 # 4 bytes
+    }
+    
+    #header["Destination"] = bytes(map(int, dest.split(".")))
+    
+    header_bytes = [
+        ((header["VERSION"] << 4) + header["IHL"] << 8) + ((header["DSCP"] << 2) + header["ECN"]),
+        header["TotLen"],
+        header["Identification"],
+        ((header["Flags"] << 13) + header["FragmentOffset"]), 
+        (header["TTL"] << 8) + header["Protocol"],
+        header["Checksum"],
+        header["Source"] & 0xFFFF,
+        header["Destination"]
+    ]
+    
+    print([hex(x) for x in header_bytes])
+    header_bytes.append(8)
+    packet = header_bytes
+    print(packet)
+    
+  
+    
+    
+
+    
+if __name__ == '__main__':
+    #fragmenttest()
+    packetTest()
