472ce640b3c563d8f507ed34ee6116241bb343aa
Vincent Palmer <vincentpalmernordfors@gmail.com>
2022-03-13 16:38:06 +0100

    Starting with fragmenting of data

--- app.py
+++ app.py
@@ -179,10 +179,31 @@ def create_packet():
 
 def fragment(data):
     
+    fragments = []
+    
     nbrParts = 0
     dataLength = len(data)
     
+    max_size = 30
     
+    if ((dataLength % max_size) == 0):
+        nbrParts = dataLength / max_size
+        
+    else:
+        nbrParts = int((dataLength - (dataLength % max_size)) / max_size) + 1
+
+        padding = [0 for _ in range(max_size - (dataLength % max_size))]
+        
+        padding[len(padding) - 1] += len(padding)
+        
+        data += bytes(padding)
+    
+    while data:
+        fragments.append(data[:max_size])
+        data = data[max_size:]
+    
+    
+    return fragments   
 
 #
 # def encrypt():
