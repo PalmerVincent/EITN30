c7e6c2826a00abcc17df372cf619d01abd2a6fbe
InternetInuti <vincent.palmer@hotmail.com>
2022-03-18 18:15:13 +0100

    Added plotter code

--- src/plotter.py
+++ src/plotter.py
@@ -3,31 +3,42 @@ import matplotlib.pyplot as plt
 
 def main():
     print("Hit")
-    delay = []
+    latency = []
     packet = []
     
-    with open("test2.txt", "r") as f:
+    with open("latency.txt", "r") as f:
         for line in f:
-            print(line)
-            l = line.split()[3].split("/")
-            print(l[1])
-            delay.append(float(l[1]))
+            l = line.split(":")[1:]
+            for i in l:
+                i = i.split()[3].split("/")[1]
+                latency.append(float(i))
     
-    with open("test.txt", "r") as f:
+    with open("packets.txt", "r") as f:
         for line in f:
-            print(line)
-            l = line.split()
-            print(l)
-            packet.append(float(l[5][:-1]))
-            
+            l = line.split(":")[1:]
+            for i in l:
+                packet.append(float(i.split()[5][:-1]))            
     
-    x = np.linspace(0.005, 1.005, 100)
+    x = np.linspace(1.005, 0.005, 100)
     
-    print(delay)
+    print(latency)
     print(packet)
-    #plt.plot(x, delay)
-    #plt.plot(x, packet)
-    #plt.show()
+    
+    fig, axs = plt.subplots(nrows=2, ncols=1)
+        
+    axs[0].plot(x, latency)
+    axs[0].set_xlim(1.005, 0.005)
+    axs[0].set_ylabel("Average latency (ms)")
+    
+    axs[1].plot(x, packet)
+    axs[1].set_xlim(1.005, 0.005)
+    axs[1].set_ylabel("Average packet loss (%)")
+    
+    axs[1].set_xlabel("Test")
+    
+    plt.show()
+
+    fig.suptitle("Plots")
 
 if __name__ == "__main__":
     main()
