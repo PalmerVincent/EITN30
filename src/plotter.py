791ce6c232c7347d3946f6bf34e70d0ba538beec
InternetInuti <vincent.palmer@hotmail.com>
2022-03-29 10:54:14 +0200

    Done

--- src/plotter.py
+++ src/plotter.py
@@ -19,20 +19,25 @@ def main():
             for i in l:
                 packet.append(float(i.split()[5][:-1]))            
     
-    x = np.linspace(1.005, 0.005, 100)
+    x = np.linspace(1.005, 0.055, 95)
+
+    x = np.append(x, np.linspace(0.052275, 0.0005, 20))
     
     print(latency)
     print(packet)
+    print(len(latency))
+    print(len(packet))
+    print(len(x))
     
     fig, axs = plt.subplots(nrows=2, ncols=1)
         
     axs[0].plot(x, latency)
-    axs[0].set_xlim(1.005, 0.005)
-    axs[0].set_ylim(-1000, 10000)
+    axs[0].set_xlim(0.03, -0.01)
+    axs[0].set_ylim(-1000, 15000)
     axs[0].set_ylabel("Average latency (ms)")
     
     axs[1].plot(x, packet)
-    axs[1].set_xlim(1.005, 0.005)
+    axs[1].set_xlim(0.03, -0.01)
     axs[1].set_ylim(-10, 100)
     axs[1].set_ylabel("Average packet loss (%)")
     
@@ -41,6 +46,7 @@ def main():
     plt.show()
 
     fig.suptitle("Plots")
+    
 
 if __name__ == "__main__":
     main()
